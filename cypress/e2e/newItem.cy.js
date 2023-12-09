9b5cd5a877df81a678072be140f7dcfb98b55f71
PMG2112 <pmgimperia@gmail.com>
2023-12-09 12:49:41 -0600

    new test

--- cypress/e2e/newItem.cy.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress"/>
-
-describe('configureNewPipelineProject_011', () => {
-   
-   it('TC_03.05.011 Verify to add and visible a Display Name', function() {
-      
-   })
-
-})   
